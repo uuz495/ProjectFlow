619feffd4027534fc1117fec09ebc6678a811267
Yuyuko <124090478@link.cuhk.edu.cn>
2026-07-15 20:10:43 +0800

    Initialize Spring Boot backend

---
+++ src/test/java/io/github/uuz495/projectflow/ProjectFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.uuz495.projectflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
